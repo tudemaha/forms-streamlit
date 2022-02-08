8bc83366900cdf8f31d69861f3bd92fafc0f233f
tudemaha <gede_mahardika88@yahoo.co.id>
2022-02-08 23:17:46 +0800

    finishing import data

--- forms-streamlit.py
+++ forms-streamlit.py
@@ -30,30 +30,54 @@ def unique(list):
 genders, cities, languages = [], [], []
 for row in rows:
     genders.append(row.gender)
-    cities.append(row.city)
+    cities.append(row.city.title())
     languages.append(row.language)
 
 # gender chart
-st.markdown("<h5>Jenis Kelamin</h5>", unsafe_allow_html=True)
+st.markdown("<h5>1. Jenis Kelamin</h5>", unsafe_allow_html=True)
 
 genders_labels = unique(genders)
 genders_sizes = [genders.count('Laki-laki'), genders.count('Perempuan')]
 
-fig1, ax1 = plt.subplots()
+fig1, ax1 = plt.subplots(figsize=(7, 4))
 ax1.pie(genders_sizes, labels=genders_labels, autopct='%1.1f%%', shadow=True, startangle=90, radius=3)
 ax1.axis('equal')
 st.pyplot(fig1)
 
+# city chart
+st.markdown("<br><h5>2. Asal Responden</h5>", unsafe_allow_html=True)
+cities_label = unique(cities)
+cities_sizes = []
+
+for city in cities_label:
+    cities_sizes.append(cities.count(city))
+
+x = np.arange(len(cities_label))
+width = 0.35
+
+fig2, ax2 = plt.subplots()
+rects = ax2.bar(x, cities_sizes, width)
+
+# add text for labels and custom x-axis tick label, etc.
+ax2.set_ylabel('Jumlah')
+ax2.set_xticks(x, cities_label)
+ax2.legend()
+
+ax2.bar_label(rects, padding=3)
+fig2.tight_layout()
+
+st.pyplot(fig2)
+
 # programming language chart
-st.markdown("<br><h5>Bahasa Pemrograman Favorit</h5>", unsafe_allow_html=True)
+st.markdown("<br><h5>3. Bahasa Pemrograman Favorit</h5>", unsafe_allow_html=True)
 
 languages_label = unique(languages)
 languages_sizes = []
 for language in languages_label:
     languages_sizes.append(languages.count(language))
 
-fig2, ax2 = plt.subplots(figsize=(6, 3), subplot_kw=dict(aspect="equal"))
-wedges, texts = ax2.pie(languages_sizes, wedgeprops=dict(width=0.5), startangle=-40)
+fig3, ax3 = plt.subplots(figsize=(6, 3), subplot_kw=dict(aspect="equal"))
+wedges, texts = ax3.pie(languages_sizes, wedgeprops=dict(width=0.5), startangle=-40)
 
 bbox_props = dict(boxstyle="square,pad=0.3", fc="w", ec="k", lw=0.72)
 kw = dict(arrowprops=dict(arrowstyle="-"), bbox=bbox_props, zorder=0, va="center")
@@ -65,13 +89,13 @@ for i, p in enumerate(wedges):
     horizontalalignment = {-1: "center", 1: "center"}[int(np.sign(x))]
     connectionstyle = "angle,angleA=0,angleB={}".format(ang)
     kw["arrowprops"].update({"connectionstyle": connectionstyle})
-    ax2.annotate(languages_label[i] + (f"\n({languages_sizes[i]})") , xy=(x, y), xytext=(1.35*np.sign(x), 1.4*y),
+    ax3.annotate(languages_label[i] + (f"\n({languages_sizes[i]})"), xy=(x, y), xytext=(1.45*np.sign(x), 1.45*y),
                 horizontalalignment=horizontalalignment, **kw)
 
-st.pyplot(fig2)
+st.pyplot(fig3)
 
 # hello world example
-st.markdown("<br><h5>Kreasi Hello World Mereka...</h5>", unsafe_allow_html=True)
+st.markdown("<br><h5>4. Kreasi Hello World Mereka...</h5>", unsafe_allow_html=True)
 
 
 for language in languages_label:
