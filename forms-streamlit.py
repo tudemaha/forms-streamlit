11f903f9f0652d6dde64430a46017dcb06717616
tudemaha <gede_mahardika88@yahoo.co.id>
2022-02-08 16:01:08 +0800

    display favorite languages and hello world

--- forms-streamlit.py
+++ forms-streamlit.py
@@ -1,6 +1,7 @@
 import streamlit as st
 from gsheetsdb import connect
 import matplotlib.pyplot as plt
+import numpy as np
 
 form_title = 'Ayo Kawan! Ayo Testing2...'
 
@@ -25,31 +26,56 @@ def unique(list):
             unique_list.append(string)
     return unique_list
 
-
-# gender chart
-st.markdown("<h5>Jenis Kelamin</h5>", unsafe_allow_html=True)
-
-genders, cities, languages, hello = [], [], [], []
-
+# store data from query into list
+genders, cities, languages = [], [], []
 for row in rows:
     genders.append(row.gender)
     cities.append(row.city)
     languages.append(row.language)
 
+# gender chart
+st.markdown("<h5>Jenis Kelamin</h5>", unsafe_allow_html=True)
+
 genders_labels = unique(genders)
 genders_sizes = [genders.count('Laki-laki'), genders.count('Perempuan')]
+
 fig1, ax1 = plt.subplots()
 ax1.pie(genders_sizes, labels=genders_labels, autopct='%1.1f%%', shadow=True, startangle=90, radius=3)
 ax1.axis('equal')
 st.pyplot(fig1)
 
 # programming language chart
-
+st.markdown("<br><h5>Bahasa Pemrograman Favorit</h5>", unsafe_allow_html=True)
 
 languages_label = unique(languages)
 languages_sizes = []
 for language in languages_label:
     languages_sizes.append(languages.count(language))
 
-st.write(languages_label)
-st.write(languages_sizes)
+fig2, ax2 = plt.subplots(figsize=(6, 3), subplot_kw=dict(aspect="equal"))
+wedges, texts = ax2.pie(languages_sizes, wedgeprops=dict(width=0.5), startangle=-40)
+
+bbox_props = dict(boxstyle="square,pad=0.3", fc="w", ec="k", lw=0.72)
+kw = dict(arrowprops=dict(arrowstyle="-"), bbox=bbox_props, zorder=0, va="center")
+
+for i, p in enumerate(wedges):
+    ang = (p.theta2 - p.theta1)/2. + p.theta1
+    y = np.sin(np.deg2rad(ang))
+    x = np.cos(np.deg2rad(ang))
+    horizontalalignment = {-1: "center", 1: "center"}[int(np.sign(x))]
+    connectionstyle = "angle,angleA=0,angleB={}".format(ang)
+    kw["arrowprops"].update({"connectionstyle": connectionstyle})
+    ax2.annotate(languages_label[i] + (f"\n({languages_sizes[i]})") , xy=(x, y), xytext=(1.35*np.sign(x), 1.4*y),
+                horizontalalignment=horizontalalignment, **kw)
+
+st.pyplot(fig2)
+
+# hello world example
+st.markdown("<br><h5>Kreasi Hello World Mereka...</h5>", unsafe_allow_html=True)
+
+
+for language in languages_label:
+    st.write(language)
+    for row in rows:
+        if row.language == language:
+            st.code(row.hello, language.lower())
